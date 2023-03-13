9f03c870c18df7015c3e5f08f9170ef5fefd99ac
Adedoyin <doyinfummi2012@gmail.com>
2023-03-13 18:55:29 +0000

    notification added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {NgToastModule} from 'ng-angular-popup';
 
@@ -17,7 +15,6 @@ import {NgToastModule} from 'ng-angular-popup';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
     HttpClientModule,
     NgToastModule
   ],
--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
